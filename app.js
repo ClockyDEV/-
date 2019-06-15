e56e2f3a4ba2540058a4504f72b0110187f0f217
ClockyDEV <51348480+ClockyDEV@users.noreply.github.com>
2019-06-14 21:44:32 -0400

    Eaeea

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ assualt.on("reconnecting", () => {
 
 assualt.on("ready", () => {
      log(`Assualt Discord Bot has started with ${assualt.users.size} users and ${assualt.guilds.size} guilds!!`)
-     assualt.user.setGame("| a!help");
+     assualt.user.setActivity("| a!help, ", "WATCHING");
 });
 
 assualt.on("message", async message => {
